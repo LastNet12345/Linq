cf871f94fe50a07310b76f2e6fd0ffd8b3faa133
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-11-15 15:14:04 +0100

    Demo

---
+++ Linq/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    //public delegate void Test(string str);
+    internal static class Extensions
+    {
+        internal static void AddYear(this Person person, int year)
+        {
+            person.Age += year;
+        }
+
+        internal static void AddOwnMethod(this List<Person> people)
+        {
+
+        } 
+        
+        internal static void AddOwnMethod2(this IEnumerable<Person> people)
+        {
+
+        }
+
+        internal static IEnumerable<T> Testing<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate is null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            foreach (var item in source)
+            {
+                if (predicate(item)) yield return item;
+            }
+        }
+    }
+}
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace Linq
+﻿using System.Linq;
+
+namespace Linq
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             var persons = GetPersons();
+
+            var res = persons.Testing(IsOver20);
+            var res2 = persons.Testing(p => p.Age > 20);
+            var res3 = persons.Where(p => p.Age > 20);
+            var res4 = persons.Where(IsOver20);
+
+            var res5 = persons
+                .Where(p => p.Name == "Pelle")
+                .Select(p => p.Age)
+                .Sum();
+
+            var res6 = persons
+                .Where(p => p.Age > 30)
+                .Select(p => new PersonDto
+                {
+                    Name = p.Name,
+                    NamesLength = p.Name.Length
+                });
+
+            var firstOlle = persons.FirstOrDefault(p => p.Name.Equals("Stina"));
+
+            foreach (var item in res)
+            {
+                Console.WriteLine(res);
+            }
+
+            var p = new Person("Kalle", 10);
+            p.AddYear(5);
+
+            Console.WriteLine(p.Age);
+
+            GetPersons().ToList().AddOwnMethod();
+
+            GetPersons().AddOwnMethod2();
+
+            var ints = Enumerable.Range(1, 100).ToArray();
+
+            var r = ints.Testing(i => i > 10);
+
+            Console.ReadLine();
+
+        }
+
+        private static bool IsOver20(Person person)
+        {
+            return person.Age > 20;
         }
 
         private static IEnumerable<Person> GetPersons()
@@ -26,7 +74,7 @@
                 new("Ida" ,  36),
                 new("Fia",   45),
                 new("Lisa",   45),
-                new("Sophia-Magdalena", 32),
+                new("Sophia-Magdalena", 10),
             };
         }
     }
